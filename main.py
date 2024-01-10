92eb4c7c17989b91263e40c1c2219b214ea31317
ryutaro <m2002r1028@icloud.com>
2024-01-11 00:35:28 +0900

    add: 型チェック

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ tifffile_path = r'data\JAXA_HRLULC_Japan_v23.12\LC_N34E135.tif'
 with TiffFile(tifffile_path) as tif:
     image = tif.asarray()
 
+print(image)  # [[13 13 13 ...  9  9  9] ... [ 1  1  1 ...  9  9  9]]
+print(image.shape)  # (12000, 12000)
+print(type(image))  # <class 'numpy.ndarray'>
+
 plt.imshow(image)
 plt.show()
