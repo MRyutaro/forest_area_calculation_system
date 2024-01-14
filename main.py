8db8c21908dcf0d2acf90c667ffcd5390819f459
ryutaro <m2002r1028@icloud.com>
2024-01-14 21:28:42 +0900

    fix: ファイル名から座標を取得する

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
-import numpy as np
-import pandas as pd
-from tifffile import TiffFile
+import glob
+import os
+import re
 
-if __name__ == '__main__':
-    tifffile_path = r'data\JAXA_HRLULC_Japan_v23.12\LC_N34E135.tif'
+if __name__ == "__main__":
+    dir_path = r"data\JAXA_HRLULC_Japan_v23.12"
 
-    with TiffFile(tifffile_path) as tif:
-        image = tif.asarray()
+    # ファイルの一覧を取得する
+    tiff_files_path = glob.glob(os.path.join(dir_path, "*.tif"))
+    # print(f"tiff_files_path: {tiff_files_path}")
 
-        # tiffファイルの中身を確認する
-        # print(image)  # [[13 13 13 ...  9  9  9] ... [ 1  1  1 ...  9  9  9]]
-        # print(image.shape)  # (12000, 12000)
-        # print(type(image))  # <class 'numpy.ndarray'>
-        # print(type(image[0][0]))  # <class 'numpy.uint8'>
+    # ファイル名の一覧を取得する
+    tiff_files_name = [os.path.basename(tiff_file_path) for tiff_file_path in tiff_files_path]
+    # print(f"tiff_files_name: {tiff_files_name}")
 
-    all_pixels = image.shape[0] * image.shape[1]
-    id_list = np.unique(image)
+    north_latitudes = []
+    east_longitudes = []
 
-    pixels = pd.DataFrame(index=id_list, columns=['count', 'ratio'])
-    for id in id_list:
-        pixels.loc[id, 'count'] = np.count_nonzero(image == id)
+    # LC_N34E135.tifについて、正規表現を使って34と135を取得する
+    pattern = r"LC_N(\d+)E(\d+).tif"
+    for tiff_file_name in tiff_files_name:
+        m = re.match(pattern, tiff_file_name)
+        if m:
+            north_latitude = m.group(1)
+            east_longitude = m.group(2)
+            # print(f"tiff_file_name: {tiff_file_name}, 北緯{north_latitude}度, 東経{east_longitude}度")
 
-    pixels['ratio'] = pixels['count'] / all_pixels
+            north_latitudes.append(north_latitude)
+            east_longitudes.append(east_longitude)
 
-    print(f"pixels: {pixels}")
+    # ソート
+    min_north_latitude = min(north_latitudes)
+    max_north_latitude = max(north_latitudes)
+    min_east_longitude = min(east_longitudes)
+    max_east_longitude = max(east_longitudes)
+
+    print(f"min_north_latitude: {min_north_latitude}")
+    print(f"max_north_latitude: {max_north_latitude}")
+    print(f"min_east_longitude: {min_east_longitude}")
+    print(f"max_east_longitude: {max_east_longitude}")
